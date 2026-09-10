c4f9acc5b69b185492514de81197d214bccf27c1
Hoa Giai <damhoagiai456@gmail.com>
2026-09-10 17:49:37 +0700

    feat: Done CRUD Account features

--- ledger/src/main/java/com/giaidev/ledger/controller/AccountController.java
+++ ledger/src/main/java/com/giaidev/ledger/controller/AccountController.java
@@ -5,12 +5,15 @@ package com.giaidev.ledger.controller;
 
 import com.giaidev.core.dto.ApiResponse;
 import com.giaidev.ledger.dto.request.AccountCreationRequest;
+import com.giaidev.ledger.dto.request.AccountUpdateRequest;
 import com.giaidev.ledger.dto.response.AccountResponse;
 import com.giaidev.ledger.enums.AccountStatus;
 import com.giaidev.ledger.service.AccountService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,6 +24,7 @@ import java.util.List;
 public class AccountController {
     private final AccountService accountService;
 
+    @PreAuthorize("hasRole('USER')")
     @PostMapping
     public ApiResponse<AccountResponse> create(
             @Valid @RequestBody AccountCreationRequest request
@@ -38,4 +42,43 @@ public class AccountController {
 
         return ApiResponse.success(accounts);
     }
+
+
+    @GetMapping("/{accountId}")
+    public ApiResponse<AccountResponse> getById(@PathVariable String accountId){
+        AccountResponse response = accountService.getById(accountId);
+
+        return ApiResponse.success(response);
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @PutMapping("/{accountId}")
+    public ApiResponse<AccountResponse> update(
+            @PathVariable String accountId,
+            @Valid @RequestBody AccountUpdateRequest request
+            ){
+        AccountResponse accountResponse = accountService.update(
+                accountId,
+                request
+        );
+
+        return ApiResponse.success(accountResponse);
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @PostMapping("/close/{accountId}")
+    public ApiResponse<AccountResponse> close(
+            @PathVariable String accountId
+    ){
+        AccountResponse accountResponse = accountService.close(
+                accountId
+        );
+
+        return ApiResponse.success(accountResponse);
+    }
+
+
+
+
+
 }
--- ledger/src/main/java/com/giaidev/ledger/service/AccountService.java
+++ ledger/src/main/java/com/giaidev/ledger/service/AccountService.java
@@ -3,21 +3,27 @@ package com.giaidev.ledger.service;
 import com.giaidev.core.exception.AppException;
 import com.giaidev.core.security.CurrentUserProvider;
 import com.giaidev.ledger.dto.request.AccountCreationRequest;
+import com.giaidev.ledger.dto.request.AccountUpdateRequest;
 import com.giaidev.ledger.dto.response.AccountResponse;
 import com.giaidev.ledger.entity.Account;
 import com.giaidev.ledger.enums.AccountStatus;
+import com.giaidev.ledger.enums.AccountType;
 import com.giaidev.ledger.exception.LedgerErrorCode;
 import com.giaidev.ledger.mapper.AccountMapper;
 import com.giaidev.ledger.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.time.Instant;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AccountService {
     private final AccountRepository accountRepository;
     private final AccountMapper accountMapper;
@@ -65,11 +71,112 @@ public class AccountService {
                     userId,
                     status
             );
+        }
+        return accountMapper.toResponse(accounts);
+    }
+
+    private Account getOwnedAccount(String accountId) {
+        String userId = currentUserProvider.getUserId();
+        return getOwnedAccount(accountId, userId);
+    }
+
+    private Account getOwnedAccount(
+            String accountId,
+            String userId
+    ) {
+        return accountRepository
+                .findByIdAndUserId(accountId, userId)
+                .orElseThrow(() -> new AppException(
+                        LedgerErrorCode.ACCOUNT_NOT_FOUND
+                ));
+    }
 
 
+
+    @Transactional(readOnly = true)
+    public AccountResponse getById(String accountId){
+        Account account = getOwnedAccount(accountId);
+
+        return accountMapper.toResponse(account);
+    }
+
+    @Transactional
+    public AccountResponse update(
+            String accountId,
+            AccountUpdateRequest request
+    ){
+        String userId = currentUserProvider.getUserId();
+
+        Account account = getOwnedAccount(accountId, userId);
+
+        if(account.getStatus() == AccountStatus.CLOSED){
+            throw new AppException(
+                    LedgerErrorCode.ACCOUNT_CLOSED
+            );
         }
 
-        return accountMapper.toResponse(accounts);
+        String normalizedName = request.name().trim();
+
+        boolean duplicatedName = accountRepository
+                .existsByUserIdAndNameIgnoreCaseAndIdNot(
+                        userId,
+                        normalizedName,
+                        accountId
+                );
+        if(duplicatedName){
+            throw new AppException(
+                    LedgerErrorCode.ACCOUNT_NAME_EXISTED
+            );
+        }
+
+        boolean allowNegative = request.allowNegative();
+
+        if(!allowNegative
+                && account.getCurrentBalance()
+                    .compareTo(BigDecimal.ZERO) < 0){
+            //If current balance < 0 and allowNegative == false => throw error
+            throw new AppException(
+                    LedgerErrorCode.NEGATIVE_BALANCE_NOT_ALLOWED
+            );
+        }
+
+        account.updateDetails(
+                normalizedName,
+                request.provider(),
+                allowNegative
+        );
+
+        try {
+            Account savedAccount = accountRepository.saveAndFlush(account);
+            return accountMapper.toResponse(savedAccount);
+        }catch (DataIntegrityViolationException exception){
+            throw new AppException(
+                    LedgerErrorCode.ACCOUNT_NAME_EXISTED
+            );
+        }
+
+    }
+
+    @Transactional
+    public AccountResponse close(String accountId){
+        Account account = getOwnedAccount(accountId);
+
+        if(account.getStatus() == AccountStatus.CLOSED){
+            return accountMapper.toResponse(account);
+        }
+
+        if (account.getCurrentBalance()
+                .compareTo(BigDecimal.ZERO) != 0) {
+            throw new AppException(
+                    LedgerErrorCode.ACCOUNT_BALANCE_NOT_ZERO
+            );
+        }
+
+        account.close(Instant.now());
+
+        Account savedAccount = accountRepository.save(account);
+
+        return accountMapper.toResponse(savedAccount);
     }
 
 
